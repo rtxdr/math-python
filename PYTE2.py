64b15578a77f1d437f6d96bb676bb10133ae05d4
rtxdr <83834829+rtxdr@users.noreply.github.com>
2021-11-03 15:04:22 +0100

    WORKING !!!!
    it works now!!!!!!!

--- PYTE2.py
+++ PYTE2.py
@@ -1,24 +1,30 @@
 import tkinter as tk
-from tkinter.constants import LEFT, RIGHT
+from tkinter.constants import BOTTOM, LEFT, RIGHT
+import FPYTE2
 #i will try to make both values in the same window so i don't complicate stuff that much
 
 rootpyte = tk.Tk()
+rootpyte.title("Value insertion...")
 tptext = tk.Label(text="insert your values")
 tpAtxt = tk.Label(text="VALUE A")
 tpBtxt = tk.Label(text="VALUE B")
-tpAent = tk.Entry()
-tpBent = tk.Entry()
+tpAent = tk.Entry(rootpyte)
+tpBent = tk.Entry(rootpyte)
 
 def resultdef():
-    print("this sucks")
+    a = int(tpAent.get())
+    b = int(tpBent.get())
+    rootpyte.destroy()
+ 
+    FPYTE2.runFPYTE2(a,b)
 
 tpFbtn = tk.Button(text="done",command=resultdef)
 
-rootpyte.pack()
 tptext.pack()
 tpAtxt.pack(side=LEFT)
 tpBtxt.pack(side=RIGHT)
 tpAent.pack(side=LEFT)
 tpBent.pack(side=RIGHT)
+tpFbtn.pack(side=BOTTOM)
 
 rootpyte.mainloop()
